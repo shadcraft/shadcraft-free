86bc1004c34dc7d8c788b0c369783ef42d2bcd72
Luis Llanes <luisllaboj@gmail.com>
2025-11-26 18:07:08 -0700

    feat: add Wandry Analytics for registry

--- proxy.ts
+++ proxy.ts
@@ -1,22 +1,25 @@
+import { captureRegistryEvent } from "@wandry/analytics-sdk";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
-import { registry } from "@/registry";
-
-// TODO: Add analytics
+import { getRegistryItem } from "@/lib/registry";
 
 export async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   if (pathname.startsWith("/r/") && pathname.endsWith(".json")) {
     const name = pathname.slice("/r/".length).replace(/\.json$/, "");
+
     try {
-      const item = registry.items.find((item) => item.name === name);
+      const item = await getRegistryItem(name);
       if (!item) return NextResponse.next();
 
       if (process.env.NODE_ENV === "production") {
+        captureRegistryEvent(request, process.env.WANDRY_REGISTRY_TOKEN!);
       }
-    } catch {}
+    } catch {
+      console.error(`[proxy.ts]: Error getting registry item ${name}`);
+    }
   }
 
   return NextResponse.next();
